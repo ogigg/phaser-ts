11ead271b5bafcd31be7980af35ec7830448f292
Oskar Gierszewski <oskar@moodup.team>
2022-10-22 20:17:20 +0200

    Add debug

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ import GameScene from './scenes/Game';
 new Phaser.Game(
   Object.assign(config, {
     scene: [GameScene],
-    physics: { default: 'arcade' },
+    physics: { default: 'arcade', arcade: { debug: true } },
   })
 );
